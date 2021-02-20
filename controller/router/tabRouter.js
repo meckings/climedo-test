8baece80b8ddf1536908741ed9ca13464d51b651
Ebuka.Maduekwe <ebuka.maduekwe@interswitchng.com>
2021-02-20 10:21:09 +0100

    tab stats

--- controller/router/tabRouter.js
+++ controller/router/tabRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {createTab, getTabById, getAllTabs, updateTab, deleteTab} = require("../tabHandler")
+const {createTab, getTabStats, getTabById, getAllTabs, updateTab, deleteTab} = require("../tabHandler")
 
 const router = express.Router();
 
 router.post("/", createTab);
+router.get("/tabStats", getTabStats);
 router.get("/:id", getTabById);
 router.get("/", getAllTabs);
 router.put("/:id", updateTab);
--- controller/tabHandler.js
+++ controller/tabHandler.js
@@ -63,22 +63,19 @@ const getAllTabs = (req, res)=>{
         });
 }
 
-const updateTab = (req, res)=>{
-    const id = req.params.id;
-    const body = req.body;
-    console.log("update tab", id);
-    Tab.updateOne({"_id": id}, body)
+const getTabStats = (req, res)=>{
+    console.log("get tab stats");
+    Tab.find()
         .then(result=>{
-            if(result==null){
-                const body = 
-                {
-                    message: "The tab you trying to update does not exist!",
-                }
-                console.log(err);
-                res.status(statusCodes.NOT_FOUND).json(body);
-            }
-            console.log("Tab updated!", result);
-            res.status(statusCodes.UPDATED).json();
+            const newResult = [];
+            result.map(val=>{
+                const obj = {
+                    "_id": val._id,
+                    "dataPointCount" : val.dataPoints.length
+                };
+                newResult.push(obj);
+            })
+            res.json(newResult);
         })
         .catch(err=>{
             const body = 
@@ -86,40 +83,88 @@ const updateTab = (req, res)=>{
                 message: "There was an error in processing your request, please review and send again.",
                 error: err._message
             }
-            console.log(err);
+            console.log(err.error);
             res.status(statusCodes.BAD_REQUEST).json(body);
         });
+}
+
+const updateTab = (req, res)=>{
+    const id = req.params.id;
+    const body = req.body;
+    console.log("update tab", id);
+    Tab.exists({"_id": id})
+    .then(res=>{
+        if (res) {
+            return Tab.updateOne({"_id": id}, body);
+        }
+        return null;
+
+    })
+    .then(result=>{
+        if(result==null){
+            const body = 
+            {
+                message: "The tab you trying to update does not exist!",
+            }
+            res.status(statusCodes.NOT_FOUND).json(body);
+        }
+        else{
+            console.log("Tab updated!", result);
+            res.status(statusCodes.UPDATED).json();
+        }
+    })
+    .catch(err=>{
+        const body = 
+        {
+            message: "There was an error in processing your request, please review and send again.",
+            error: err._message
+        }
+        console.log(err);
+        res.status(statusCodes.BAD_REQUEST).json(body);
+    });
+        
 }
 
 const deleteTab = (req, res)=>{
     const id = req.params.id;
     console.log("delete tab", id);
-    Tab.deleteOne({"_id": id})
-        .then(result=>{
-            if(result==null){
-                const body = 
-                {
-                    message: "The tab you trying to delete does not exist!",
-                }
-                console.log(err);
-                res.status(statusCodes.NOT_FOUND).json(body);
-            }
-            console.log("Tab updated!", result);
-            res.status(statusCodes.OK).json({message: "Tab deleted successfully."});
-        })
-        .catch(err=>{
+    Tab.exists({"_id": id})
+    .then(res=>{
+        if (res) {
+            return Tab.deleteOne({"_id": id})
+        }
+        return null;
+
+    })
+    .then(result=>{
+        if(result==null){
             const body = 
             {
-                message: "There was an error in processing your request, please review and send again.",
-                error: err._message
+                message: "The tab you trying to delete does not exist!",
             }
             console.log(err);
-            res.status(statusCodes.BAD_REQUEST).json(body);
-        });
+            res.status(statusCodes.NOT_FOUND).json(body);
+        }
+        else{
+            console.log("Tab updated!", result);
+            res.status(statusCodes.OK).json({message: "Tab deleted successfully."});
+        }
+    })
+    .catch(err=>{
+        const body = 
+        {
+            message: "There was an error in processing your request, please review and send again.",
+            error: err._message
+        }
+        console.log(err.errors);
+        res.status(statusCodes.BAD_REQUEST).json(body);
+    });
+        
 }
 
 module.exports ={
     createTab,
+    getTabStats,
     getTabById,
     getAllTabs,
     updateTab,
